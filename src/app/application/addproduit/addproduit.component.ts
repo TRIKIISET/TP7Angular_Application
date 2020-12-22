c832d9a66fccf879830a472b207a408e51ad976f
Amel TRIKI <amel.triki.iset@gmail.com>
2020-12-22 13:55:33 +0100

    TP7 prêt

---
+++ src/app/application/addproduit/addproduit.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addproduit',
+  templateUrl: './addproduit.component.html',
+  styleUrls: ['./addproduit.component.css']
+})
+export class AddproduitComponent implements OnInit {
+
+  /*produit:Produit = {
+    id:0,
+    libelle:"",
+    nouveau: true,
+    madeIn:"Tunisie",
+    categorie:"Fourniture"
+  }*/
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+
+}
+
+
